0cee5f27b9bf77cf8f716549024edb4f6a57e72f
Oleksii Vorochenko <vorochenko@gmail.com>
2023-06-13 17:15:16 +0300

    add auth by creds

---
+++ mockedData/users.ts
@@ -0,0 +1,23 @@
+export const users = [
+  {
+    id: '1',
+    email: 'user1@gmail.com',
+    name: 'User User',
+    password: '12345',
+    role: 'admin',
+  },
+  {
+    id: '2',
+    email: 'admin@admin.com',
+    name: 'Admin',
+    password: '12345',
+    role: 'admin',
+  },
+  {
+    id: '3',
+    email: 'guest@mail.com',
+    name: 'Guest User',
+    password: '12345',
+    role: 'guest',
+  },
+];
